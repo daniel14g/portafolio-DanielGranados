7d4330a3d3877b5545fb19a940fd9d022570daa7
Daniel Granados <75320650+daniel14g@users.noreply.github.com>
2021-03-17 19:59:49 -0500

    Add files via upload

---
+++ java/codigo-pp.js
@@ -0,0 +1,28 @@
+
+function contActive() {
+    var selector = document.getElementById("cont-colors");
+    var selector2 = document.getElementById("btn-elemento");
+    selector.classList.toggle("active");
+    selector2.classList.toggle("active");
+}
+
+function bgWhite() {
+    var selector = document.getElementById("body");
+    selector.classList.toggle("white");
+}
+function bgYellow() {
+    var selector = document.getElementById("body");
+    selector.classList.toggle("yellow");
+}
+function bgGreen() {
+    var selector = document.getElementById("body");
+    selector.classList.toggle("green");
+}
+function bgPurple() {
+    var selector = document.getElementById("body");
+    selector.classList.toggle("purple");
+}
+function bgRed() {
+    var selector = document.getElementById("body");
+    selector.classList.toggle("red");
+}
